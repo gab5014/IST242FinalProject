cce8a31d4678a0777f76eb99aca675cb910e19c1
gab5014 <gab5014@psu.edu>
2016-11-28 23:21:37 -0500

    trying to fix panels still

--- IST242FinalProjectGroup1/src/InstructionsPanel.java
+++ IST242FinalProjectGroup1/src/InstructionsPanel.java
@@ -2,27 +2,17 @@
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Master
- */
 public class InstructionsPanel extends JPanel{
     
-    JLabel instructionsText;
-    BorderLayout layout;
-    
+//    JLabel instructionsText;
+//    BorderLayout layout;
+//    
     InstructionsPanel(){
-        layout = new BorderLayout();
-        setLayout(layout);
-        //instructionsText.setText("Stay back from enemies navigate the maze");
-        //add(instructionsText);
+//        layout = new BorderLayout();
+//        setLayout(layout);
+//        instructionsText.setText("Stay back from enemies navigate the maze");
+//        add(instructionsText);
     }
 }
