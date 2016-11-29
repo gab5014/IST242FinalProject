5bdb576d46726e21e02c36face9f3de2609fbc99
gab5014 <gab5014@psu.edu>
2016-11-29 18:16:33 -0500

    MVC navigation complete

--- IST242FinalProjectGroup1/src/OptionsPanel.java
+++ IST242FinalProjectGroup1/src/OptionsPanel.java
@@ -1,5 +1,7 @@
 
 import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
@@ -10,31 +12,53 @@ public class OptionsPanel extends JPanel{
     private JSlider difficultySlider;
     private JLabel characterNameLabel;
     private JTextField characterNameField;
+    private JButton back, setInfo;
+    private GridLayout grid;
     private Model model;
-    private Controller controller;
     
     public OptionsPanel(){
-    GridLayout grid = new GridLayout(3,1);
-    setLayout(grid);
-    
-    difficultySlider = new JSlider(JSlider.HORIZONTAL, 1, 20, 10);
-    difficultySlider.setMajorTickSpacing(1);
-    difficultySlider.setPaintTicks(true);
-    difficultySlider.setPaintLabels(true);
-    add(difficultySlider);
-    
-    characterNameLabel = new JLabel("Character Name: ");
-    add(characterNameLabel);
-    
-    characterNameField = new JTextField("Enter Name");
-}
-  
+        grid = new GridLayout(3,2);
+        setLayout(grid);
+
+        difficultySlider = new JSlider(JSlider.HORIZONTAL, 1, 20, 10);
+        difficultySlider.setMajorTickSpacing(1);
+        difficultySlider.setPaintTicks(true);
+        difficultySlider.setPaintLabels(true);
+        add(difficultySlider);
+
+        characterNameLabel = new JLabel("Character Name: ");
+        add(characterNameLabel);
+
+        characterNameField = new JTextField("Enter Name");
+        add(characterNameField);
+        
+        back = new JButton("Back");
+        add(back);
+        
+        setInfo = new JButton("Set Info");
+        add(setInfo);
+        
+    }
+    public void addActionListeners(ActionListener al){
+        this.back.addActionListener(al);
+        this.setInfo.addActionListener(al);
+    }
+    public JButton getBack(){
+        return this.back;
+    }
+    public JButton getInfo(){
+        return this.setInfo;
+    }
     public int getDifficulty(){
         return difficultySlider.getValue();
     }
-    
     public String getName(){
+        System.out.println(characterNameField.getText());
         return characterNameField.getText();
     }
+    public void setNameLabel(String name){
+        this.characterNameLabel.setText(name);
+    }
+    
     
 }
